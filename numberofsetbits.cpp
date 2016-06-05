f9f0598b2aaea1264b45be5f1e9cbde32c7278cf
Rishabh <rishabh@freshdesk.com>
2016-06-05 18:11:37 +0530

    Adding my first practice question

---
+++ numberofsetbits.cpp
@@ -0,0 +1,28 @@
+//Write an efficient program to count number of 1s in binary representation of an integer.
+
+
+
+#include <iostream>
+using namespace std;
+int count_one (int n)
+{ int cnt;
+    while(n)
+    {
+        n = n & (n-1);
+        cnt++;
+    }
+    return cnt;
+}
+
+int main()
+{
+    int T,a,val;
+    cin>>T;
+   while(T--)
+   {cin>>a;
+    val=count_one(a);
+    cout<<val<<endl;
+   	
+   }
+   return 0;
+}
